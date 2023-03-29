fc4b7a33d47f22be398902d14ccce3a49e87624b
Axel Jimenez <axely7@hotmail.com>
2023-03-29 11:02:30 -0600

    carrousel an flatlist component

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import {Movie} from '../interfaces/movieInterface';
 
 interface Props {
   movie: Movie;
+  height?: number;
+  width?: number;
+  marginTop?: number;
 }
 
-export const MoviePoster = ({movie}: Props) => {
+export const MoviePoster = ({
+  movie,
+  height = 420,
+  width = 300,
+  marginTop,
+}: Props) => {
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
     <View
       style={{
-        width: 300,
-        height: 420,
+        width,
+        height,
+        marginTop,
+        marginHorizontal: 6,
       }}>
       <View style={styles.imageContainer}>
         <Image
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {ActivityIndicator, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {MoviePoster} from '../components/MoviePoster';
 import {useMovies} from '../hooks/useMovies';
+import Carousel from 'react-native-snap-carousel';
+import {HorizontalSlider} from '../components/HorizontalSlider';
+
+const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
   const {peliculasEnCine, isLoading} = useMovies();
@@ -17,8 +27,25 @@ export const HomeScreen = () => {
   }
 
   return (
-    <View style={{marginTop: top + 20}}>
-      <MoviePoster movie={peliculasEnCine[0]} />
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20}}>
+        <View
+          style={{
+            height: 460,
+          }}>
+          <Carousel
+            data={peliculasEnCine}
+            renderItem={({item}: any) => (
+              <MoviePoster movie={item} marginTop={16} />
+            )}
+            sliderWidth={windowWidth}
+            itemWidth={300}
+            inactiveSlideOpacity={0.9}
+          />
+        </View>
+        {/* Peliculas populares */}
+        <HorizontalSlider title="En Cine" movies={peliculasEnCine} />
+      </View>
+    </ScrollView>
   );
 };
